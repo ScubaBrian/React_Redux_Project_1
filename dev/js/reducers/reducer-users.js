de1222835db7a51ab3cdd8346a5a884818b512c3
ScubaBrian <SVAD@iMac.local>
2017-10-12 16:31:24 -0400

    Initial commit
    Initial commit of react/redux project

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,25 @@
+export default function() {
+  return [
+    {
+       id: 1,
+       first: 'Brian',
+       last: 'Goers',
+       age: 43,
+       description: 'Brian loves to eat'
+    },
+    {
+       id: 2,
+       first: 'Nastya',
+       last: 'Goers',
+       age: 43,
+       description: 'Nastya loves to exercise'
+    },
+    {
+       id: 3,
+       first: 'SVAD',
+       last: 'Goers',
+       age: 26,
+       description: 'Kids love to play video games'
+    }
+  ]
+}
